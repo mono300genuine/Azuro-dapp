4c4b192642a1788e3d5588a513c092b84b1ffbe1
mono300genuine <monolinker31@gmail.com>
2025-03-25 12:20:48 -0700

    fix : middleware.ts

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,15 +1,18 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
+const env = process.env.NODE_ENV
 export default async function middleware(req: NextRequest) {
-  const country = req.geo?.country || req.headers.get('x-vercel-ip-country');
-//   const allowedCountries = ['IN', 'UK', 'CA']; // Replace with your allowed countries
-
-  const unallowedCountries = ['RU', 'CN', 'US', 'TR']; // Add unallowed countries here
-
-  if (unallowedCountries.includes(country ?? '')) {
-    return NextResponse.redirect(new URL('/restrict', req.url));
+  const res = NextResponse.next()
+  const pathname = req.nextUrl.pathname
+  // Skip this middleware in development or if the path is international
+  if (env !== 'development' && pathname !== '/international') {
+    const country = req.geo?.country || req.headers.get('x-vercel-ip-country')
+    // Here you can add the list of countries you want to allow, I have added IN and US for now
+    if (!['IN', 'US'].includes(country ?? '')) {
+      // Redirect to the international page if the country is not IN or US
+      return NextResponse.redirect(new URL('/restricted', req.url))
+    }
   }
-
-  return NextResponse.next();
+  return res
 }
-
